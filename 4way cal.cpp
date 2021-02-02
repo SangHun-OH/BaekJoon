5821d0c68a9d7d5ae954c788615922359d532ce5
Sanghun Oh <74748487+SangHun-OH@users.noreply.github.com>
2021-02-02 23:26:20 +0900

    Add files via upload

---
+++ 4way cal.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {	
+	int a;
+	cin >> a;
+
+	if (a >= 90)
+		cout << 'A';
+	else if (90 > a && a >= 80)
+		cout << 'B';
+	else if (80 > a && a>= 70)
+		cout << 'C';
+	else if (70 > a && a >= 60)
+		cout << 'D';
+	else
+		cout << 'F';
+}
