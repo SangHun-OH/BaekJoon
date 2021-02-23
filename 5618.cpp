66a9ffdde27efdc0f43e463fa4fbef1023b5b996
Sanghun Oh <74748487+SangHun-OH@users.noreply.github.com>
2021-02-23 13:25:29 +0900

    Add files via upload

---
+++ 5618.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, a, b, c;
+
+    cin >> n;
+    
+    if(n=2)
+    	cin >> a >> b;
+    else
+    	cin >> a >> b >> c;
+    	
+    for(int i = 1; i <= a; i++)
+    	if(a % i == 0 && b % i == 0 && c % i == 0) 
+    		cout << i << endl;
+}
