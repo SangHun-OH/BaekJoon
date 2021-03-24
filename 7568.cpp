ab3e6283a13b309c95dd0fcffade3611769cbed9
Sanghun Oh <74748487+SangHun-OH@users.noreply.github.com>
2021-03-25 00:09:55 +0900

    Update 7568.cpp

--- 7568.cpp
+++ 7568.cpp
@@ -15,7 +15,6 @@ int main() {
 		cin >> arr[i][0] >> arr[i][1];
 		result[i] = 1;		
 	}
-
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1])
