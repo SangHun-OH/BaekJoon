49131f924e617a65da5372f74fc5509af3b90891
오상훈 <oshosh12@gmail.com>
2021-12-08 20:24:36 +0900

    joon

--- 1931.cpp
+++ 1931.cpp
@@ -1,26 +1,23 @@
-#include <iostream>
-#include <cmath>
+#include iostream
+#include vector
+#include algorithm
+
 using namespace std;
-
 int main() {
-	int M, N;
-	int rt;
-	cin >> M >> N;
-
-	for (int i = M; i <= N; i++) {
-		rt = sqrt(i);
-		if (rt == 1 && i != 1) {	//2,3인 경우
-			cout << i << '\n';
-			continue;
-		}
-		if (i % 2) {	//홀수일 경우
-			for (int j = 2; j <= rt; j++) {
-				if (!(i%j))
-					break;
-				if (j == rt) {
-					cout << i << '\n';
-				}
-			}
+	int n;
+	cin  n;
+	vectorpairint, int a(n);
+	for (int i = 0; i  n; i++) {
+		cin  a[i].second  a[i].first;
+	}
+	sort(a.begin(), a.end());
+	int savetime = 0, ans = 0;
+	for (int i = 0; i  n; i++) {
+		if (savetime = a[i].second) {
+			savetime = a[i].first;
+			ans++;
 		}
 	}
+	printf(%d, ans);
+
 }
