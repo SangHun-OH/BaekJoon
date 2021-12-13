d2fbde87b2ace92aa9159802317e46530c5f8b34
오상훈 <oshosh12@gmail.com>
2021-12-13 19:20:18 +0900

    1629

---
+++ 1629.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+long long A, B, C;
+long long POW(int A, int B, int C) {
+if (B == 0) return 1;
+long long temp = POW(A, B/2, C);
+if (B % 2 == 0) return temp; //짝수 일때
+else return temp * A % C; //홀 수 일때
+}
+int main(void) {
+	cin >> A >> B >> C;
+	cout << POW(A, B, C);
+	return 0;
+}
